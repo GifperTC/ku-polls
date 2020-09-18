30c091692634650ce4f8bc8d4e5891742f5be4e6
Nutta Sittipongpanich <59831047+GifperTC@users.noreply.github.com>
2020-09-19 02:38:34 +0700

    Iteration2 features and config

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Web application for conducting online polls and surveys.
 
 ## Project Documents
-***
+
 [Vision Statement](https://github.com/GifperTC/ku-polls/wiki/Vision-Statement)
 
 [Requirements](https://github.com/GifperTC/ku-polls/wiki/Requirements)
--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -14,7 +14,7 @@ class QuestionModelTests(TestCase):
         is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=time)
+        future_question = Question(pub_date=time, end_date=time + datetime.timedelta(hours=10))
         self.assertIs(future_question.was_published_recently(), False)
 
     def test_was_published_recently_with_old_question(self):
@@ -23,7 +23,7 @@ class QuestionModelTests(TestCase):
         is older than 1 day.
         """
         time = timezone.now() - datetime.timedelta(days=1, seconds=1)
-        old_question = Question(pub_date=time)
+        old_question = Question(pub_date=time, end_date=time + datetime.timedelta(hours=10))
         self.assertIs(old_question.was_published_recently(), False)
 
     def test_was_published_recently_with_recent_question(self):
@@ -32,19 +32,70 @@ class QuestionModelTests(TestCase):
         is within the last day.
         """
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
-        recent_question = Question(pub_date=time)
-        self.assertIs(recent_question.was_published_recently(), True)
+        recent_question = Question(pub_date=time, end_date=time + datetime.timedelta(hours=10))
+        self.assertIs(recent_question.was_published_recently(), True)    
 
-    def create_question(question_text, days):
+    def test_is_published(self):
         """
-        Create a question with the given `question_text` and published the
-        given number of `days` offset to now (negative for questions published
-        in the past, positive for questions that have yet to be published).
+        is_published() returns True for questions whose pub_date
+        is before or is the current time.
         """
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
+        time = timezone.now()
+        recent_question = Question(pub_date=time, end_date=time + datetime.timedelta(hours=10))
+        self.assertIs(True, recent_question.is_published())
+
+    def test_is_published_many_cases(self):
+        """
+        is_published() returns True for questions whose pub_date is already passed.
+        """
+        time = timezone.now()
+
+        question1 = Question(pub_date=time-datetime.timedelta(hours=10), end_date=time+datetime.timedelta(hours=10))
+        self.assertIs(True, question1.is_published())
+
+        question2 = Question(pub_date=time, end_date=time++ datetime.timedelta(hours=10))
+        self.assertIs(True, question2.is_published())
+
+        question3 = Question(pub_date=time+datetime.timedelta(hours=10), end_date=time+datetime.timedelta(hours=20))
+        self.assertIs(False ,question3.is_published())
+
+        question4 = Question(pub_date=time+datetime.timedelta(hours=10), end_date=time)
+        self.assertIs(False, question4.is_published())
+
+    def test_can_vote(self):
+        """
+        can_vote() returns True for questions whose pub_date is already passed current time and end_date is in the future.
+        """
+        time = timezone.now()
+        question1 = Question(pub_date=time, end_date=time+datetime.timedelta(hours=10))
+        self.assertIs(True, question1.can_vote())
+
+        question2 = Question(pub_date=time-datetime.timedelta(hours=10), end_date=time+datetime.timedelta(hours=10))
+        self.assertIs(True, question2.can_vote())
+
+        question3 = Question(pub_date=time+datetime.timedelta(hours=10), end_date=time+datetime.timedelta(hours=20))
+        self.assertIs(False, question3.can_vote())
+
+        question4 = Question(pub_date=time+datetime.timedelta(hours=10), end_date=time)
+        self.assertIs(False, question4.can_vote())
+        
+        question5 = Question(pub_date=time, end_date=time)
+        self.assertIs(False, question5.can_vote())
+
+        question6 = Question(pub_date=time-datetime.timedelta(hours=10), end_date=time)
+        self.assertIs(False, question6.can_vote())
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time, end_date=time + datetime.timedelta(hours=10))
 
 class QuestionIndexViewTests(TestCase):
+    
     def test_no_questions(self):
         """
         If no questions exist, an appropriate message is displayed.
